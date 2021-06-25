e5cc70e3db7fb3032855c240b90582ebfc28e91d
Marco Florian Aigner <m.f.aigner@googlemail.com>
2021-06-25 07:09:16 +0200

    setup prisma orm
    Setup an initial prisma schema and prisma client to work with the full stack

--- Backend/API/app.js
+++ Backend/API/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const {PrismaClient} = require('@prisma/client');
 const morgan = require('morgan');
 const app = express();
+const prisma = new PrismaClient()
 const PORT = process.env.PORT || 8081
 
 //Parse the bodies of incoming requests as json-data
@@ -10,8 +12,22 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Proves that the server works fine by responding with 'Test'
-app.get('/', (req, res, next) => {
-    res.send('Test');
+app.get('/', async (req, res, next) => {
+    const messages = await prisma.test.findMany();
+    res.send(messages);
+})
+
+app.get('/:id', async(req, res, next) => {
+    const id = Number(req.params.id);
+    const message = await prisma.test.findUnique({
+        where: {id}
+    });
+    res.send(message);
+})
+
+app.post('/', async (req, res, next) => {
+    const newMessage = await prisma.test.create({data:{message: req.body.message}});
+    res.status(201).send(newMessage);
 })
 
 app.listen(PORT, '0.0.0.0', () => {
