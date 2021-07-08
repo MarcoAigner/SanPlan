aaf47d4d45032599908727190e3c4d787c573c5c
Marco Florian Aigner <m.f.aigner@googlemail.com>
2021-07-08 18:22:21 +0200

    update mockdata inserts
    update the mockdata inserts to comply to the new data model:
    
    adresses and articles now posess an attribute to keep track on how many times they have been used (used for sorting in frontend).
    A service now requires to have a responsible person

--- Backend/Datenbank/mockdata.sql
+++ Backend/Datenbank/mockdata.sql
@@ -2,13 +2,21 @@ INSERT INTO `sanplanba`.`address` (`address`, `street`, `house_number`, `postal_
 INSERT INTO `sanplanba`.`address` (`address`, `street`, `house_number`, `postal_code`, `city`) VALUES ('BRK Kreisverband Neu-Ulm', 'Pfaffenweg', '42', '89231', 'Neu-Ulm');
 
 INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Gerolt', 'Steiner');
-INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Volker', 'Putt');
 INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Sunny', 'Täter');
+INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Ernst', 'Fall');
+INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Lisa', 'Bonn');
+INSERT INTO `sanplanba`.`person` (`first_name`, `last_name`) VALUES ('Otto', 'Päde');
 
-INSERT INTO `sanplanba`.`service` (`address_id`, `title`, `start`, `end`, `active`) VALUES ('2', 'Dienstabend: Zeltaufbau', '2021-06-29 19:00:00', '2021-06-29 21:00:00', '0');
-INSERT INTO `sanplanba`.`service` (`address_id`, `title`, `start`, `end`) VALUES ('1', 'SanWD: Ulm vs. Oldenburg', '2021-06-30 15:00:00', '2021-06-30 19:00:00');
+INSERT INTO `sanplanba`.`service` (`address_id`, `responsible`, `title`, `start`, `end`, `active`) VALUES ('2', '3', 'Dienstabend: Zeltaufbau', '2021-06-29 19:00:00', '2021-06-29 21:00:00', '0');
+UPDATE `sanplanba`.`address` SET times_used = times_used + 1 WHERE id = 1;
+
+INSERT INTO `sanplanba`.`service` (`address_id`, `responsible`, `title`, `start`, `end`) VALUES ('1', '4', 'SanWD: Ulm vs. Oldenburg', '2021-06-30 15:00:00', '2021-06-30 19:00:00');
+UPDATE `sanplanba`.`address` SET times_used = times_used + 1 WHERE id = 2;
 
 INSERT INTO `sanplanba`.`medical_service` (`service_id`, `number`, `contact_person`, `annotations`) VALUES ('2', '#1/21', 'Herr Müller', 'Seiteneingang verwenden');
 
 INSERT INTO `sanplanba`.`article_usage` (`service_id`, `person_id`, `article_id`, `quantity`, `time`) VALUES ('2', '1', '235', '1', '2021-06-29 16:45:00');
-INSERT INTO `sanplanba`.`article_usage` (`service_id`, `person_id`, `article_id`, `quantity`, `time`) VALUES ('2', '3', '145', '1', '2021-06-29 17:20:00');
+UPDATE `sanplanba`.`article` SET times_used = times_used + 1 WHERE id = 235;
+
+INSERT INTO `sanplanba`.`article_usage` (`service_id`, `person_id`, `article_id`, `quantity`, `time`) VALUES ('2', '3', '144', '1', '2021-06-29 17:20:00');
+UPDATE `sanplanba`.`article` SET times_used = times_used + 1 WHERE id = 144;
