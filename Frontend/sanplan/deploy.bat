26bbaaa686e44de306886a4a3919743d7ebef3bd
Marco Florian Aigner <m.f.aigner@googlemail.com>
2021-07-11 19:11:21 +0200

    enable history mode in deployment

--- Frontend/sanplan/deploy.bat
+++ Frontend/sanplan/deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/marcoaigner/SanPlan.git master:gh-pages&&cd ..
+npm run build&&cd dist&&copy index.html 404.html&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/marcoaigner/SanPlan.git master:gh-pages&&cd ..
