5351994393a278deb2b11e11c866a7efd0487982
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2022-02-08 18:55:39 +0100

    deploy

---
+++ precache-manifest.7c1cb3d4299ac6485a174011a138892f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cdbe91d692ab9ecbff",
+    "url": "/SanPlan/css/chunk-vendors.1f7dab17.css"
+  },
+  {
+    "revision": "e900fb9c3bc239f7e032e7b40fff8699",
+    "url": "/SanPlan/index.html"
+  },
+  {
+    "revision": "b6017824b9d388ada955",
+    "url": "/SanPlan/js/app.166a4b5d.js"
+  },
+  {
+    "revision": "a1cdbe91d692ab9ecbff",
+    "url": "/SanPlan/js/chunk-vendors.3a3f06fb.js"
+  },
+  {
+    "revision": "ed601a37108cc357d8b4085b19050713",
+    "url": "/SanPlan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SanPlan/robots.txt"
+  }
+]);
